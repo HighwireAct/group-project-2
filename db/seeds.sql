3144aa9fb3bae825bbc79bc7d82c81d81b92ae1c
rhoang57 <rhoang57@gmail.com>
2019-01-21 12:59:04 -0800

    add seeds.sql in db directory, worked on quiz.handlebars file, create api routes to get post delete

---
+++ db/seeds.sql
@@ -0,0 +1,48 @@
+INSERT INTO subjects (subject)
+VALUES 
+    ("Math"),
+    ("Computing");
+
+INSERT INTO topics (topic, SubjectId)
+VALUES
+    ("Early math", 1),
+    ("Algebra 1", 1),
+    ("Hour of Code", 2),
+    ("Hour of Code", 2);
+
+INSERT INTO subtopics (subtopic, TopicId)
+VALUES
+    ("Counting", 1),
+    ("Solving Equations", 2),
+    ("Creating Webpages", 3),
+    ("Color Science", 4);
+
+INSERT INTO tests (testname, SubtopicId, TopicId)
+VALUES
+    ("Counting to 10", 1, 1),
+    ("Find the variable value", 2, 2),
+    ("HTML basics", 3, 3),
+    ("Color 101", 3, 4);
+
+INSERT INTO questions (question, answer1, answer2, answer3, answer4, correctAnswer, TestId)
+VALUES 
+    ("What number comes immediately after 5?", "1", "2", "4", "6", 4, 1),
+    ("What number comes immediately after 6?", "7", "8", "10", "1", 1, 1),
+    ("What number comes immediately after 3?", "3", "9", "7", "4", 4, 1),
+    ("What number comes immediately before 5?", "1", "2", "4", "6", 3, 1),
+    ("What number comes immediately before 10?", "8", "2", "3", "9", 4, 1),
+    ("Select the equation where n = 6 is the solution.", "12 = n + 5", "2n = 14", "3n - 1 = 17", "6 = n + 1", 3, 2),
+    ("Select the equation where n = 1 is the solution.", "12 = n + 11", "2n = 14", "3n - 1 = 17", "6 = n + 1", 1, 2),
+    ("Select the equation where n = 0 is the solution.", "15 = n - 5", "2n = 14", "10 - 2n = 10", "6 = n + 1", 3, 2),
+    ("Select the equation where n = 4 is the solution.", "12 = n + 5", "2n = 14", "3n - 1 = 11", "6 = n + 1", 3, 2),
+    ("Select the equation where n = 8 is the solution.", "12 = n + 5", "2n = 16", "3n - 1 = 17", "6 = n + 1", 2, 2),
+    ("HTML _________ are defined with the <p> tags.", "pages", "positions", "paragraphs", "people", 3, 3),
+    ("HTML _________ are defined with the <br> tags.", "brown fonts", "bridges", "line breaks", "indentions", 3, 3),
+    ("By default, which tag would produce the largest font size?.", "<h1>", "<h2>", "<h3>", "<h6>", 1, 3),
+    ("Which of the following start tags do NOT require a corresponding end tag? ", "<title>", "<p>", "<body>", "<br>", 4, 3),
+    ("The <h1> element defines a _________________", "header", "headline", "high-point", "hover", 2, 3),
+    ("Which of the following is not a property of color?", "texture", "saturation", "value", "hue", 1, 4),
+    ("Which color property refers to what the actual color is in the rainbow spectrum?", "opacity", "hue", "classification", "brilliance", 2, 4),
+    ("How bright a color is relative to something else is often referred to a color's ________________", "worth", "comparison", "value", "spectrum", 3, 4),
+    ("Saturation refers to the ________________ of a color.", "fullness", "power", "liquidity", "intensity", 4, 4),
+    ("The color that we perceive through our sight is most influenced by _______________.", "peer pressure", "Crayola naming conventions", "art teachers", "light", 4, 4);
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,86 @@
 var db = require("../models");
 
 module.exports = function(app) {
+  //get all subjects
+  app.get("/api/subjects", function(req, res) {
+    db.Subject.findAll({}).then(function(dbSubjects) {
+      res.json(dbSubjects);
+    });
+  });
+
+  //----------------------------------------------------------------
+
+  //get all topics
+  app.get("/api/topics", function(req, res) {
+    db.Topic.findAll({}).then(function(dbTopics) {
+      res.json(dbTopics);
+    });
+  });
+
+  //----------------------------------------------------------------
+
+  //get all subtopics
+  app.get("/api/subtopics", function(req, res) {
+    db.Subtopic.findAll({}).then(function(dbSubtopics) {
+      res.json(dbSubtopics);
+    });
+  });
+
+  //----------------------------------------------------------------
+
+  //get all tests
+  app.get("/api/tests", function(req, res) {
+    db.Test.findAll({}).then(function(dbTests) {
+      res.json(dbTests);
+    });
+  });
+
+  // Create a new test
+  app.post("/api/tests", function(req, res) {
+    db.Test.create(req.body).then(function(dbTest) {
+      res.json(dbTest);
+    });
+  });
+
+  // Delete a test by id
+  app.delete("/api/tests/:id", function(req, res) {
+    db.Test.destroy({ where: { id: req.params.id } }).then(function(dbTest) {
+      res.json(dbTest);
+    });
+  });
+
+  //----------------------------------------------------------------
+
+  //get all questions
+  app.get("/api/questions", function(req, res) {
+    db.Question.findAll({}).then(function(dbQuestions) {
+      res.json(dbQuestions);
+    });
+  });
+
+  // Create a new question
+  app.post("/api/questions", function(req, res) {
+    db.Question.create(req.body).then(function(dbQuestion) {
+      res.json(dbQuestion);
+    });
+  });
+
+  // Delete a question by id
+  app.delete("/api/questions/:id", function(req, res) {
+    db.Question.destroy({ where: { id: req.params.id } }).then(function(dbQuestion) {
+      res.json(dbQuestion);
+    });
+  });
+
+  //----------------------------------------------------------------
+
   // Get all examples
   app.get("/api/examples", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
     });
   });
-
+  
   // Create a new example
   app.post("/api/examples", function(req, res) {
     db.Example.create(req.body).then(function(dbExample) {
